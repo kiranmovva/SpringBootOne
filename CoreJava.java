e2baeea887f5edb6167f5cb6b1e526aa265d7a9d
kiranmovva <kiranmovva.java@gmail.com>
2018-02-25 20:17:39 -0500

    find first repeated leatter

---
+++ CoreJava.java
@@ -0,0 +1,25 @@
+package lamda.com;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoreJava {
+	public static void main (String[] args){
+	// first repaeteda element
+		//ArrayindexOutOfBoundsException if you give i<=
+		String st1="FGTNVVVVVVVVVGFHHHHHHHHHHHHHH";
+		char[] dtr2=st1.toCharArray();
+	     Map<String,Integer> abc=new HashMap<String,Integer>();
+		for(int i=0 ;i<st1.toCharArray().length;i++){
+                if(abc.containsKey(String.valueOf(dtr2[i]))){
+                	System.out.println("element is " +dtr2[i]);
+                    break;    	
+                }else{
+                	abc.put(String.valueOf(dtr2[i]), new Integer(1));
+                }
+		}
+	}
+
+}
+
+
